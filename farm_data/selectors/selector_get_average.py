72625f26c7190fc878d264de060462e649e6d9f9
barth007 <bartholomew.bassey@st.futminna.edu.ng>
2024-08-30 13:13:19 +0100

    added the average graph endpoint

--- farm_data/selectors/selector_get_average.py
+++ farm_data/selectors/selector_get_average.py
@@ -1,5 +1,7 @@
-from django.db.models import Avg
+from django.db.models import Avg, Max, Min
 from django.db.models.functions import TruncDay
+from django.utils import timezone
+from datetime import timedelta
 from farm_data.models import SoilData, EnvironmentalData
 from typing import Dict, List
 
@@ -7,18 +9,34 @@ def get_average_soil_data_per_day() -> List[Dict[str, float]]:
     """
     This function returns the average soil data per day
     """
-    return SoilData.objects.annotate(day=TruncDay('created_at')).values('day').annotate(
+    today = timezone.now().date()
+    last_7_days = today - timedelta(days=7)
+    return SoilData.objects.filter(created_at__date__gte=last_7_days).annotate(day=TruncDay('created_at')).values('day').annotate(
         avg_soil_temp=Avg('soilTemperature'),
+        max_soil_temp=Max('soilTemperature'),
+        min_soil_temp=Min('soilTemperature'),
         avg_moisture=Avg('moisture'),
-        avg_ph=Avg('phLevel')
+        max_moisture=Max('moisture'),
+        min_moisture=Min('moisture'),
+        avg_ph=Avg('phLevel'),
+        max_ph=Max('phLevel'),
+        min_ph=Min('phLevel')
     ).order_by('day')
 
 def get_average_environmental_data_per_day() -> List[Dict[str, float]]:
     """
     This function returns the average environmental data per day
     """
-    return EnvironmentalData.objects.annotate(day=TruncDay('created_at')).values('day').annotate(
+    today = timezone.now().date()
+    last_7_days = today - timedelta(days=7)
+    return EnvironmentalData.objects.filter(created_at__date__gte=last_7_days).annotate(day=TruncDay('created_at')).values('day').annotate(
         avg_environ_temp=Avg('temperature'),
+        max_environ_temp=Max('temperature'),
+        min_environ_temp=Min('temperature'),
         avg_environ_pressure=Avg('pressure'),
-        avg_environ_humidity=Avg('humidity')
+        max_environ_pressure=Max('pressure'),
+        min_environ_pressure=Min('pressure'),
+        avg_environ_humidity=Avg('humidity'),
+        max_environ_humidity=Max('humidity'),
+        min_environ_humidity=Min('humidity')
     ).order_by('day')
--- farm_data/views/views_average.py
+++ farm_data/views/views_average.py
@@ -1,30 +1,78 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+import calendar
 from farm_data.selectors.selector_get_average import get_average_soil_data_per_day, get_average_environmental_data_per_day
+from typing import List, Dict, Any
 
 class AverageDataPerDayView(APIView):
+    """ 
+    A view that returns the average data per day.
+    Methods:
+        - get: Retrieves the average data per day.
+    Attributes:
+        - soil_temperature: A list of dictionaries containing the average, maximum, and minimum soil temperature per day.
+        - ph: A list of dictionaries containing the average, maximum, and minimum pH per day.
+        - moisture: A list of dictionaries containing the average, maximum, and minimum moisture per day.
+        - environmental_temperature: A list of dictionaries containing the average, maximum, and minimum environmental temperature per day.
+        - humidity: A list of dictionaries containing the average, maximum, and minimum humidity per day.
+        - pressure: A list of dictionaries containing the average, maximum, and minimum pressure per day.
     """
-    This class is used to get the average data per day
-    """
+    
     def get(self, request, *args, **kwargs):
         """
         Handle GET requests.
         Parameters:
-        - request: The HTTP request object.
-        - args: Additional positional arguments.
-        - kwargs: Additional keyword arguments.
+            - request: The HTTP request object.
+            - args: Additional positional arguments.
+            - kwargs: Additional keyword arguments.
         Returns:
-        - Response: The HTTP response object with the average data per day.
-        """
+            - Response: The HTTP response object with the average data per
+       """
         soil_data = get_average_soil_data_per_day()
         environmental_data = get_average_environmental_data_per_day()
+         
+        def order_days(data: List[Dict[str, Any]]) -> List[Dict[str, Any]]:
+            """
+            Orders the data by day of the week.
+            Args:
+            data (list): A list of dictionaries containing day and other data.
+            Returns:
+            list: A list of dictionaries ordered by day of the week, excluding days with no data.
+            """
+        
+            ordered_data = {day: None for day in calendar.day_name}
+            for entry in data:
+                day_name = entry['day'].strftime('%A')
+                ordered_data[day_name] = entry
+            return [ordered_data[day] for day in calendar.day_name if ordered_data[day] is not None]
+            
+       
+        def format_data(data: List[Dict[str, float]], keys: Dict[str, str]) -> List[Dict[str, Dict[str, float]]]:
+            """
+            Formats the given data into a list of dictionaries containing average, maximum, and minimum values for each day.
+            Args:
+                data (list): The data to be formatted.
+                keys (dict): A dictionary containing keys for average, maximum, and minimum values.
+            Returns:
+                list: A list of dictionaries containing formatted data for each day.
+            """
+            return [
+                {
+                    day['day'].strftime('%A'): {
+                        'avg': day.get(keys['avg']),
+                        'max': day.get(keys['max']),
+                        'min': day.get(keys['min'])
+                    }
+                } for day in order_days(data)
+            ]
+
         response_data = {
-            'soil_temperature': [{day['day'].strftime('%A'): day.get('avg_soil_temperature')} for day in soil_data],
-            'ph': [{day['day'].strftime('%A'): day.get('avg_ph')} for day in soil_data],
-            'moisture': [{day['day'].strftime('%A'): day.get('avg_moisture') for day in soil_data}],
-            'environmental_temperature': [{day['day'].strftime('%A'): day.get('avg_temperature') for day in environmental_data}],
-            'humidity': [{day['day'].strftime('%A'): day.get('avg_humidity')} for day in environmental_data],
-            'pressure': [{day['day'].strftime('%A'): day.get('avg_pressure')} for day in environmental_data],
+            'soil_temperature': format_data(soil_data, {'avg': 'avg_soil_temp', 'max': 'max_soil_temp', 'min': 'min_soil_temp'}),
+            'ph': format_data(soil_data, {'avg': 'avg_ph', 'max': 'max_ph', 'min': 'min_ph'}),
+            'moisture': format_data(soil_data, {'avg': 'avg_moisture', 'max': 'max_moisture', 'min': 'min_moisture'}),
+            'environmental_temperature': format_data(environmental_data, {'avg': 'avg_temperature', 'max': 'max_temperature', 'min': 'min_temperature'}),
+            'humidity': format_data(environmental_data, {'avg': 'avg_humidity', 'max': 'max_humidity', 'min': 'min_humidity'}),
+            'pressure': format_data(environmental_data, {'avg': 'avg_pressure', 'max': 'max_pressure', 'min': 'min_pressure'}),
         }
         return Response(response_data, status=status.HTTP_200_OK)
